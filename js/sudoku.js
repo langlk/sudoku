133109321cdc3fcc0df72eb787dc0c6d83a8ef2d
Kelsey Langlois <kels.langlois@gmail.com>
2017-10-12 12:53:51 -0700

    Update isLegal to test if subsquares are legal.

--- js/sudoku.js
+++ js/sudoku.js
@@ -7,8 +7,8 @@ export class Sudoku {
     let columnNumbers;
     let rowNumbers;
     for (let i = 0; i < 9; i++) {
-      columnNumbers = new Array(9);
-      rowNumbers = new Array(9);
+      let columnNumbers = new Array(9);
+      let rowNumbers = new Array(9);
       for (let j = 0; j < 9; j++) {
         if (columnNumbers[this.board[i][j]]) {
           return false;
@@ -19,6 +19,22 @@ export class Sudoku {
           return false;
         } else {
           rowNumbers[this.board[j][i]] = true;
+        }
+      }
+    }
+    let subSquareNumbers;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        subSquareNumbers = new Array(9);
+        for (let k = 0; k < 3; k++) {
+          for (let l = 0; l < 3; l++) {
+            if (subSquareNumbers[this.board[(i * 3) + k][(j * 3) + l]]) {
+              console.log('square covered')
+              return false;
+            } else {
+              subSquareNumbers[this.board[(i * 3) + k][(j * 3) + l]] = true;
+            }
+          }
         }
       }
     }
--- spec/sudoku-spec.js
+++ spec/sudoku-spec.js
@@ -28,5 +28,33 @@ describe('Sudoku', function() {
       let sudoku = new Sudoku(board);
       expect(sudoku.isLegal()).toEqual(false);
     });
+
+    it("returns false if a subsquare does not have the numbers 1-9 once each", function() {
+      let board = [];
+      for (let i = 0; i < 9; i++) {
+        board.push([]);
+        for (let j = 0; j < 9; j++) {
+          board[i][(j + i) % 9] = j + 1;
+        }
+      }
+      let sudoku = new Sudoku(board);
+      expect(sudoku.isLegal()).toEqual(false);
+    });
+
+    it("returns true if all rows, columns, and subsquares have the numbers 1-9 once each", function() {
+      let board = [
+        [1,7,4,2,8,5,3,6,9],
+        [8,3,5,6,9,4,2,1,7],
+        [6,9,2,1,7,3,8,4,5],
+        [2,4,6,5,1,7,9,8,3],
+        [3,8,1,9,6,2,5,7,4],
+        [7,5,9,3,4,8,1,2,6],
+        [5,6,3,4,2,1,7,9,8],
+        [4,1,7,8,5,9,6,3,2],
+        [9,2,8,7,3,6,4,5,1]
+      ];
+      let sudoku = new Sudoku(board);
+      expect(sudoku.isLegal()).toEqual(true);
+    });
   });
 });
